ee7f78fab4d1473a30e2fa7429e375816c7e4bea
Maija Haapakangas <mhaapakangas@gmail.com>
2019-03-24 23:10:17 +0200

    Add unit tests for Maksukortti and fix toString

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,61 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertNotNull(kortti);
+    }
+
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(127);
+        assertEquals(137, kortti.saldo());
+    }
+
+    @Test
+    public void otaRahaaVahentaaSaldoaOikein() {
+        kortti.otaRahaa(9);
+        assertEquals(1, kortti.saldo());
+    }
+
+    @Test
+    public void otaRahaaVahentaaSaldoaOikeinKunRahaaTasmalleenOikeaMaara() {
+        kortti.otaRahaa(10);
+        assertEquals(0, kortti.saldo());
+    }
+
+    @Test
+    public void otaRahaaEiMuutaSaldoaKunRahaaLiianVahan() {
+        kortti.otaRahaa(11);
+        assertEquals(10, kortti.saldo());
+    }
+
+    @Test
+    public void otaRahaaPalauttaaTrueKunKortilleJaaRahaa() {
+        assertTrue(kortti.otaRahaa(9));
+    }
+
+    @Test
+    public void otaRahaaPalauttaaTrueKunRahaaTasmalleenOikeaMaara() {
+        assertTrue(kortti.otaRahaa(10));
+    }
+
+    @Test
+    public void otaRahaaPalauttaaFalseKunRahaaLiianVahan() {
+        assertFalse(kortti.otaRahaa(11));
+    }
+
+    @Test
+    public void toStringPalauttaaOikein(){
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void toStringPalauttaaOikeinKunSenttejaAlle10(){
+        kortti = new Maksukortti(105);
+        assertEquals("saldo: 1.05", kortti.toString());
     }
 }
