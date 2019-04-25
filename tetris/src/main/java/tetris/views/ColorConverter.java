e7398fcc25c4ae4885cc0c6abae728bd1e64b745
Maija Haapakangas <mhaapakangas@gmail.com>
2019-04-26 01:05:57 +0300

    check style fix

--- tetris/src/main/java/tetris/views/ColorConverter.java
+++ tetris/src/main/java/tetris/views/ColorConverter.java
@@ -7,15 +7,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ColorConverter {
-    private static Map<Integer, Color> colorMapping = new HashMap<Integer, Color>() {{
-        put(BlockColor.PINK.ordinal(), Color.HOTPINK);
-        put(BlockColor.BLUE.ordinal(), Color.CORNFLOWERBLUE);
-        put(BlockColor.GREEN.ordinal(), Color.PALEGREEN);
-        put(BlockColor.PURPLE.ordinal(), Color.MEDIUMPURPLE);
-        put(BlockColor.YELLOW.ordinal(), Color.LEMONCHIFFON);
-        put(BlockColor.ORANGE.ordinal(), Color.ORANGE);
-        put(BlockColor.TURQUOISE.ordinal(), Color.MEDIUMTURQUOISE);
-    }};
+    private static Map<Integer, Color> colorMapping = new HashMap<Integer, Color>() {
+        {
+            put(BlockColor.PINK.ordinal(), Color.HOTPINK);
+            put(BlockColor.BLUE.ordinal(), Color.CORNFLOWERBLUE);
+            put(BlockColor.GREEN.ordinal(), Color.PALEGREEN);
+            put(BlockColor.PURPLE.ordinal(), Color.MEDIUMPURPLE);
+            put(BlockColor.YELLOW.ordinal(), Color.LEMONCHIFFON);
+            put(BlockColor.ORANGE.ordinal(), Color.ORANGE);
+            put(BlockColor.TURQUOISE.ordinal(), Color.MEDIUMTURQUOISE);
+        }
+    };
 
     public static Color convert(int color) {
         return colorMapping.getOrDefault(color, Color.GRAY);
